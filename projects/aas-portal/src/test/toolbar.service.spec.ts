0c087fabe1e2321cd65cd770d202b44f04bd2dd6
Ralf Aron <raron@phoenixcontact.com>
2023-10-16 15:43:03 +0200

    make aasportal updatable

--- projects/aas-portal/src/test/toolbar.service.spec.ts
+++ projects/aas-portal/src/test/toolbar.service.spec.ts
@@ -23,20 +23,33 @@ describe('ToolbarService', () => {
         expect(service).toBeTruthy();
     });
 
-    it('sets a new toolbar', function(done: DoneFn) {
-        const template = jasmine.createSpyObj<TemplateRef<unknown>>(['createEmbeddedView']);
-        service.set(template);
-        service.toolbarTemplate.pipe(first(value => value !== null)).subscribe(value => {
-            expect(value).toEqual(template);
-            done();
-        })
+    describe('set', function () {
+        beforeEach(function() {
+            service.clear();
+        });
+
+        it('sets a new toolbar', function (done: DoneFn) {
+            const template = jasmine.createSpyObj<TemplateRef<unknown>>(['createEmbeddedView']);
+            service.set(template);
+            service.toolbarTemplate.pipe(first(value => value !== null)).subscribe(value => {
+                expect(value).toEqual(template);
+                done();
+            })
+        });
     });
 
-    it('removes a toolbar', function(done: DoneFn) {
-        service.clear();
-        service.toolbarTemplate.pipe(first()).subscribe(value => {
-            expect(value).toBeNull();
-            done();
-        })
+    describe('clear', function () {
+        beforeEach(function () {
+            const template = jasmine.createSpyObj<TemplateRef<unknown>>(['createEmbeddedView']);
+            service.set(template);
+        });
+
+        it('removes a toolbar', function (done: DoneFn) {
+            service.clear();
+            service.toolbarTemplate.pipe(first(value => value === null)).subscribe(value => {
+                expect(value).toBeNull();
+                done();
+            })
+        });
     });
 });
